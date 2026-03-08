54cc8705e36fd19ff57f74efb7918fa61215f72a
Milton Denys <milton.silva17@fatec.sp.gov.br>
2026-03-08 00:10:16 -0300

    feat: simplify development scripts by removing Netlify CLI dependencies and updating commands

--- dev.bat
+++ dev.bat
@@ -1,20 +1,11 @@
 @echo off
-REM Script para executar em modo de desenvolvimento com Netlify Functions (Windows)
+REM Script para executar em modo de desenvolvimento (Windows)
 
 REM Instalar dependências se necessário
-if not exist "node_modules\" (
+if not exist "node_modules" (
   echo Instalando dependencias...
   call npm install
 )
 
-REM Instalar Netlify CLI se necessário
-where netlify >nul 2>nul
-if errorlevel 1 (
-  echo Instalando Netlify CLI...
-  call npm install -g netlify-cli
-)
-
-REM Executar servidor de desenvolvimento com Netlify Functions
-echo Iniciando servidor de desenvolvimento...
-call netlify dev
-
+REM Executar servidor de desenvolvimento
+call npm run dev
--- dev.sh
+++ dev.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-# Script para executar em modo de desenvolvimento com Netlify Functions
+# Script para executar em modo de desenvolvimento
 
 # Instalar dependências se necessário
 if [ ! -d "node_modules" ]; then
   npm install
 fi
 
-# Instalar Netlify CLI se necessário
-if ! command -v netlify &> /dev/null; then
-  npm install -g netlify-cli
-fi
-
-# Executar servidor de desenvolvimento com Netlify Functions
-netlify dev
-
+# Executar servidor de desenvolvimento
+npm run dev
